ad0f657988acfb72321bf8a4261b3bdeaaa1b222
arthas <arthas@kali.kali>
2024-05-26 03:21:27 -0400

    Lesson #1, Homework #4

---
+++ UrbanUniversity/lesson1/homework4.py
@@ -0,0 +1,9 @@
+my_string = input("Введите текст: ")
+
+new_string = my_string.replace(' ', '')
+
+print(my_string.upper())   # String in UPPERCASE
+print(my_string.lower())   # String in lowercase
+print(new_string)          # String without spaces
+print(my_string[0])        # First symbol
+print(my_string[-1])       # Last symbol
