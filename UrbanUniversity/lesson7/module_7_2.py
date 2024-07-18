d411c754c2be20212cf2920a29b62c2c52e17274
arthas <arthas@kali.kali>
2024-07-18 06:49:58 -0400

    Lesson #7. Homework 2

---
+++ UrbanUniversity/lesson7/module_7_2.py
@@ -0,0 +1,21 @@
+def custom_write(filename, strings):
+    res = {}
+    with open(filename, 'w', newline='\r\n') as f:
+        for i, line in enumerate(strings, 1):
+            res[(i, f.tell())] = line
+            f.write(line + '\n')
+
+    return res
+
+
+if __name__ == '__main__':
+    info = [
+        'Text for tell.',   
+        'Используйте кодировку utf-8.',
+        'Because there are 2 languages!',
+        'Спасибо!'
+    ]
+
+    result = custom_write('test.txt', info)
+    for elem in result.items():
+        print(elem)
