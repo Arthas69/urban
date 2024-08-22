8580e71cd74e6c0e3010adfacd85bf89ef851146
arthas <arthas@kali.kali>
2024-08-22 15:36:19 -0400

    Lesson #10. Homework 2

---
+++ UrbanUniversity/lesson10/module_10_2.py
@@ -0,0 +1,32 @@
+from threading import Thread
+from time import sleep
+
+
+class Knight(Thread):
+    def __init__(self, name, strength):
+        super().__init__()
+        self.name = name.title()
+        self.strength = strength
+        self.enemies = 100
+
+    def run(self):
+        days = 0
+        print(f'{self.name}, you are under attack')
+        while True:
+            if self.enemies <= 0:
+                print(f'{self.name} wins after {days} days!')
+                break
+            self.enemies -= self.strength
+            days += 1
+            sleep(1)
+            print(f'{self.name} fights for {max(days, 0)}..., {self.enemies} enemies remaining')
+
+
+# Создание класса
+first_knight = Knight('Sir Lancelot', 10)
+second_knight = Knight("Sir Galahad", 20)
+# Запуск потоков и остановка текущего
+# Вывод строки об окончании сражения
+
+first_knight.start()
+second_knight.start()
