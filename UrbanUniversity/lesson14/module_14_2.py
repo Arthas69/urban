cd003422adb98de4d2b64b388045ec24b1aa7d3d
arthas <arthas@kali.kali>
2024-10-07 15:32:11 -0400

    Lesson #14. Homework 2

---
+++ UrbanUniversity/lesson14/module_14_2.py
@@ -0,0 +1,49 @@
+import sqlite3
+
+DB_NAME = 'not_telegram.db'
+
+
+with open(DB_NAME, 'w'):
+    pass
+
+
+db = sqlite3.connect(DB_NAME)
+cursor = db.cursor()
+
+cursor.execute("""CREATE TABLE IF NOT EXISTS users (
+id INTEGER PRIMARY KEY AUTOINCREMENT,
+username TEXT NOT NULL,
+email TEXT NOT NULL UNIQUE,
+age INTEGER,
+balance INTEGER NOT NULL
+)""")
+
+
+for i in range(1, 11):
+    cursor.execute("INSERT INTO users (username, email, age, balance) VALUES (?, ?, ?, ?)",
+                   (f'User{i}', f'example{i}@gmail.com', i * 10, 1000))
+
+
+cursor.execute("UPDATE users SET balance = ? WHERE mod(id, 2) <> ?", (500, 0))
+
+cursor.execute("DELETE FROM users WHERE mod(id, 3) == ?", (1, ))
+
+db.commit()
+
+cursor.execute("SELECT username, email, age, balance FROM users WHERE age != ?", (60,))
+
+all_data = cursor.fetchall()
+for row in all_data:
+    print("Имя: {} | Почта: {} | Возраст: {} | Баланс: {}".format(*row))
+
+cursor.execute("DELETE FROM users WHERE id == 6")
+
+db.commit()
+
+total_users = cursor.execute("SELECT COUNT(*) FROM users").fetchone()[0]  # Total number of users in DB
+total_balance = cursor.execute("SELECT SUM(balance) FROM users").fetchone()[0]  # Sum of balances in DB
+avg_balance = total_balance / total_users  # Average balance
+
+print(avg_balance)
+
+db.close()
